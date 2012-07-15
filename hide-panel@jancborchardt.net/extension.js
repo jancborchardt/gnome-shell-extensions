fddd4723b783f9c3d51521b0d2281b814970cb79
Jan-Christoph Borchardt <hey@jancborchardt.net>
2012-07-15 03:26:09 -0700

    also hide the now useless activities button

--- hide-panel@jancborchardt.net/extension.js
+++ hide-panel@jancborchardt.net/extension.js
@@ -2,27 +2,29 @@
 // freely licensed under MIT
 
 const Overview = imports.ui.main.overview;
-const Panel = imports.ui.main.panel.actor;
+const Panel = imports.ui.main.panel;
 
 function init() {
 }
 
 function enable() {
     hidePanel();
+    Panel._activitiesButton.actor.hide();
     Overview.connect('hiding', hidePanel);
     Overview.connect('showing', showPanel);
 }
 
 function disable() {
     showPanel();
+    Panel._activitiesButton.actor.show();
     Overview.disconnect(Overview.connect('hiding', hidePanel));
     Overview.disconnect(Overview.connect('showing', showPanel));
 }
 
 function hidePanel() {
-    Panel.hide();
+    Panel.actor.hide();
 }
 
 function showPanel() {
-    Panel.show();
+    Panel.actor.show();
 }
